37647a6935505ea285278b5fcaaada7bd6e3298d
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-03-13 21:21:32 +0000

    Modificación en la configuración del contenedor

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from infrastructure import Repository, get_session, Categoria
 
 with get_session() as session:
     categoria_repo = Repository[Categoria](Categoria, session)
-    categoria_repo.add(Categoria(nombre="Yoga"))
+    print(categoria_repo.get_by_id(1))
