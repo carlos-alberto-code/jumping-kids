5b7e67bac57ccb6e783b60f73615afe6ae0a05c4
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-03-13 16:22:47 +0000

    Eliminación de una importación no usada.

--- infrastructure/database/models.py
+++ infrastructure/database/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-from sqlalchemy.sql import func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Boolean, Integer, String, ForeignKey, DateTime, Column, Table, CheckConstraint
 
